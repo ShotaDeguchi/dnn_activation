848c15dc8d8dfe6a3f1bf9217c9c2f9b555f411f
shota <shotadeguchio2o5@gmail.com>
2022-04-20 01:36:14 +0900

    add code

--- 01_torch/dnn.py
+++ 01_torch/dnn.py
@@ -4,6 +4,42 @@ deep neural network
 ********************************************************************************
 """
 
+import os
 import time
 import numpy as np
 import torch
+from torch import nn
+
+class DNN(nn.Module):
+    def __init__(
+        self, 
+        f_in, f_out, f_hid, depth, 
+        w_init, b_init, act, 
+        lr = 5e-4, opt = "Adam", f_scl = "minmax", 
+        d_type = "float32", r_seed = 1234
+    ):
+        # init
+        super().__init__()
+        self.f_in   = f_in
+        self.f_out  = f_out
+        self.f_hid  = f_hid
+        self.depth  = depth
+        self.w_init = w_init
+        self.b_init = b_init
+        self.act    = act
+        self.lr     = lr
+        self.opt    = opt
+        self.f_scl  = f_scl
+        self.d_type = d_type
+        self.r_seed = r_seed
+        # self.device = device
+        self.setup(r_seed, d_type)
+
+    def setup(
+        self, d_type, r_seed
+    ):
+        self.d_type
+        self.r_seed
+
+
+
