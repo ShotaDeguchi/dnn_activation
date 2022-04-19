2674cb8ee3bdd8f29f44daa05db5d85aefbc7f53
shota <shotadeguchio2o5@gmail.com>
2022-04-19 22:03:31 +0900

    add function

---
+++ 01_torch/function.py
@@ -0,0 +1,26 @@
+"""
+********************************************************************************
+functions to be learned
+********************************************************************************
+"""
+
+import numpy as np
+
+def func1(x):
+    y = .6 * np.sin(np.pi * x) \
+        + .3 * np.sin(2. * np.pi * x) \
+        + .1 * np.cos(8. * np.pi * x)
+    return y
+
+def func2(x):
+    if x <= 0:
+        y = .2 * np.sin(6. * np.pi * x)
+    else:
+        y = 1. + .1 * x * np.cos(12. * np.pi * x)
+    return y
+
+def func3(x, y):
+    z = .6 * np.sin(np.pi * x) 
+    z = .6 * np.sin(np.pi * y) 
+    return z
+
