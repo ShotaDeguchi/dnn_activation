907d7ab5284246671ac38280921bc7eb1512ca06
shota <shotadeguchio2o5@gmail.com>
2022-04-21 20:33:11 +0900

    add code

--- 00_tf2/infer.py
+++ 00_tf2/infer.py
@@ -4,3 +4,18 @@ inference
 ********************************************************************************
 """
 
+import os
+import time
+import numpy as np
+import tensorflow as tf
+
+from dnn import *
+
+def __main__():
+
+
+
+
+if __name__ == "__main__":
+    __main__()
+
