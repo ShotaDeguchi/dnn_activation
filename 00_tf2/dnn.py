5ac8d08dafcd7b96067a4af8b7cad39527828676
shota <shotadeguchio2o5@gmail.com>
2022-04-21 12:41:37 +0900

    add code

--- 00_tf2/dnn.py
+++ 00_tf2/dnn.py
@@ -8,5 +8,23 @@ import time
 import numpy
 import tensorflow as tf
 
-class dnn
+class dnn_1D(tf.keras.Model):
+    def __init__(
+        self, 
+        x, y, 
+        
+    ):
+        # initialization
+
+
+
+
+class dnn_2D(tf.keras.Model):
+    def __init__(
+        self, 
+        x, y, z, 
+        
+    ):
+        # initialization
+
 
