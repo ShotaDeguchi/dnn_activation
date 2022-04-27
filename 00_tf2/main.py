9edcdf54b98f2ed5454cc788694d27c02231120b
shota <shotadeguchio2o5@gmail.com>
2022-04-27 23:14:20 +0900

    add code

--- 00_tf2/main.py
+++ 00_tf2/main.py
@@ -60,8 +60,8 @@ def main():
     # y_train = tf.sin(np.pi * x_train)
 
     # define model
-    act = "swish"
-    model = DNN(
+    act = "relu"
+    model_relu = DNN(
         x_train, y_train, 
         f_in, f_out, f_hid, depth, 
         w_init, b_init, act, 
@@ -70,16 +70,48 @@ def main():
     )
     # train
     with tf.device("/device:GPU:0"):
-        model.train(n_epc = int(1e4), n_btc = -1, c_tol = 1e-5)
+        model_relu.train(n_epc = int(1e4), n_btc = -1, c_tol = 1e-5)
     # infer
-    y_infer = model.infer(x_infer)
+    y_relu = model_relu.infer(x_infer)
+
+    # define model
+    act = "tanh"
+    model_tanh = DNN(
+        x_train, y_train, 
+        f_in, f_out, f_hid, depth, 
+        w_init, b_init, act, 
+        lr, opt, f_scl, 
+        d_type, r_seed
+    )
+    # train
+    with tf.device("/device:GPU:0"):
+        model_tanh.train(n_epc = int(1e4), n_btc = -1, c_tol = 1e-5)
+    # infer
+    y_tanh = model_tanh.infer(x_infer)
+
+    # define model
+    act = "swish"
+    model_swish = DNN(
+        x_train, y_train, 
+        f_in, f_out, f_hid, depth, 
+        w_init, b_init, act, 
+        lr, opt, f_scl, 
+        d_type, r_seed
+    )
+    # train
+    with tf.device("/device:GPU:0"):
+        model_swish.train(n_epc = int(1e4), n_btc = -1, c_tol = 1e-5)
+    # infer
+    y_swish = model_swish.infer(x_infer)
 
     # compare
     if p_id == 0:
         plt.figure(figsize=(4, 4))
-        plt.plot(x, y, label="function", alpha=.7, linestyle="-", c="k")
-        plt.plot(x_infer, y_infer, label="dnn", alpha=.7, linestyle="--", c="r")
+        plt.plot(x, y, label="function", alpha=.7, linestyle="-", lw = 3, c="k")
         plt.scatter(x_train, y_train, alpha=.7, marker="x", c="r")
+        plt.plot(x_infer, y_relu, label="relu", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_tanh, label="tanh", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_swish, label="swish", alpha=.7, linestyle="--")
         plt.xlim(-1.2, 1.2)
         plt.ylim(-1.2, 1.2)
         plt.grid(alpha=.5)
@@ -87,9 +119,11 @@ def main():
         plt.savefig("./figures/problem" + str(p_id) + ".jpg")
     elif p_id == 1:
         plt.figure(figsize=(4, 4))
-        plt.plot(x, y, label="function", alpha=.7, linestyle="-", c="k")
-        plt.plot(x_infer, y_infer, label="dnn", alpha=.7, linestyle="--", c="r")
+        plt.plot(x, y, label="function", alpha=.7, linestyle="-", lw = 3, c="k")
         plt.scatter(x_train, y_train, alpha=.7, marker="x", c="r")
+        plt.plot(x_infer, y_relu, label="relu", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_tanh, label="tanh", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_swish, label="swish", alpha=.7, linestyle="--")
         plt.xlim(-1.2, 1.2)
         plt.ylim(-1.2, 1.2)
         plt.grid(alpha=.5)
@@ -97,11 +131,13 @@ def main():
         plt.savefig("./figures/problem" + str(p_id) + ".jpg")
     elif p_id == 2:
         plt.figure(figsize=(4, 4))
-        plt.plot(x, y, label="function", alpha=.7, linestyle="-", c="k")
-        plt.plot(x_infer, y_infer, label="dnn", alpha=.7, linestyle="--", c="r")
+        plt.plot(x, y, label="function", alpha=.7, linestyle="-", lw = 3, c="k")
         plt.scatter(x_train, y_train, alpha=.7, marker="x", c="r")
+        plt.plot(x_infer, y_relu, label="relu", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_tanh, label="tanh", alpha=.7, linestyle="--")
+        plt.plot(x_infer, y_swish, label="swish", alpha=.7, linestyle="--")
         plt.xlim(-1.2, 1.2)
-        plt.ylim(-1.2, 1.2)
+        plt.ylim(-2.5, 2.5)
         plt.grid(alpha=.5)
         plt.legend(loc="upper left")
         plt.savefig("./figures/problem" + str(p_id) + ".jpg")
