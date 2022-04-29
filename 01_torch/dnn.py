eecaea95c3da89683b469d0219af221b05bf63c9
shota <shotadeguchio2o5@gmail.com>
2022-04-29 14:59:18 +0900

    add code

--- 01_torch/dnn.py
+++ 01_torch/dnn.py
@@ -51,10 +51,10 @@ class DNN(nn.Module):
     def dnn_init(
         self, f_in, f_out, f_hid, depth
     ):
-        arch = nn.Sequential(
+        network = nn.Sequential(
             nn.Linear(f_in, f_hid)
         )
-        return arch
+        return network
 
     def forward_pass(
         self, x
@@ -71,4 +71,58 @@ class DNN(nn.Module):
         
         # y = 
         # return y
+
+    def train(
+        self, n_epc, n_btc, c_tol, es_pat
+    ):
+        print(">>>>> train")
+        print("         n_epoch :", n_epc)
+        print("         n_batch :", n_btc)
+        print("         c_tlrnc :", c_tol)
+        print("         patience:", es_pat)
+
+        wait = 0
+        loss_best = 9999
+        t0 = time.time()
+        # if n_btc == -1:
+        #     print(">>>>> executing full-batch training")
+        #     for epc in range(n_epc):
+        #         loss_epc = 0.
+        #         loss_epc = self.grad_desc(self.x, self.y)
+        #         self.loss_log.append(loss_epc)
+
+        #         # monitor 
+        #         if epc % 100 == 0:
+        #             elps = time.time() - t0
+        #             print("epc: %d, loss: %.6e, elps: %.3f"
+        #                 % (epc, loss_epc, elps))
+        #             t0 = time.time()
+
+        #         # # save 
+        #         # if epc % 100 == 0:
+        #         #     self.save(self.save_path + "model" + str(epc))
+
+        #         # early stopping
+        #         if loss_epc < loss_best:
+        #             loss_best = loss_epc
+        #             wait = 0
+        #         else:
+        #             if wait >= es_pat:
+        #                 print(">>>>> early stopping")
+        #                 break
+        #             wait += 1
+
+        #         # terminate if converged
+        #         if loss_epc < c_tol:
+        #             print(">>>>> converging to the tolerance")
+        #             break
+
+        # else:
+        #     print("\n>>>>> executing mini-batch training")
+        #     raise NotImplementedError(">>>>> train")
+
+    def infer(
+        self, x
+    ):
+        print(">>>>> infer")
 
