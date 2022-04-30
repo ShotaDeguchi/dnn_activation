1b990d1ad01bdb2561bbd86fe74496da20762fb8
shota <shotadeguchio2o5@gmail.com>
2022-04-30 23:46:31 +0900

    add code

--- 01_torch/dnn.py
+++ 01_torch/dnn.py
@@ -45,10 +45,10 @@ class DNN(nn.Module):
         self.y = y
 
         # for feature scaling
-        XY = tf.concat([x, y], 1)
-        self.lower = tf.cast(tf.reduce_min (XY, axis = 0), dtype=self.d_type)
-        self.upper = tf.cast(tf.reduce_max (XY, axis = 0), dtype=self.d_type)
-        self.mean  = tf.cast(tf.reduce_mean(XY, axis = 0), dtype=self.d_type)
+        XY = torch.cat((x, y), dim = 1)
+        self.lower = tf.cast(tf.reduce_min (XY, axis=0), dtype=self.d_type)
+        self.upper = tf.cast(tf.reduce_max (XY, axis=0), dtype=self.d_type)
+        self.mean  = tf.cast(tf.reduce_mean(XY, axis=0), dtype=self.d_type)
 
         # build a deep neural network
         self.w_init = self.weight_init(self.w_init, self.r_seed)
